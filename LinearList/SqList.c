1785bc1d1d192e24cd5f8aca554f299d8c1b5d50
usotsuki <black-sword-k@outlook.com>
2021-07-30 20:48:12 +0800

    添加插入和按值删除功能

--- LinearList/SqList.c
+++ LinearList/SqList.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define MaxSize 100
 #define InitSize 30
 #define IncreaseSize 10
 
@@ -11,7 +10,7 @@ typedef int ElemType;
 typedef struct SqList
 {
     ElemType *data;
-    int length;
+    int MaxSize, length;
 } SqList;
 
 // 初始化线性表
@@ -23,11 +22,13 @@ void DestroyList(SqList *L);
 // 打印线性表
 void PrintList(SqList L);
 
+void IncreaseList(SqList *L);
+
 // 增
 bool ListInsert(SqList *L, ElemType e);
 
 // 删
-bool ListDelete(SqList *L, ElemType *e);
+bool ListDelete(SqList *L, ElemType e);
 
 // 查
 bool LocateElem(SqList L, ElemType e);
@@ -36,12 +37,22 @@ int main(void)
 {
     SqList L;
     InitList(&L);
-    
+
     for (int i = 0; i < InitSize; i++)
     {
         L.data[i] = i;
+        L.length += 1;
     }
 
+    PrintList(L);
+
+    ListInsert(&L, 10);
+    ListInsert(&L, 89);
+
+    PrintList(L);
+
+    ListDelete(&L, 89);
+    ListDelete(&L, 15);
     PrintList(L);
 
     return 0;
@@ -49,15 +60,66 @@ int main(void)
 
 void InitList(SqList *L)
 {
-    L->data = (ElemType *) malloc (sizeof(ElemType)*InitSize);
-    L->length = InitSize;
+    L->data = (ElemType *)malloc(sizeof(ElemType) * InitSize);
+    L->length = 0;
+    L->MaxSize = InitSize;
 }
 
-void PrintList(SqList L) {
+void PrintList(SqList L)
+{
     for (int i = 0; i < L.length; i++)
     {
         printf("%4d", L.data[i]);
     }
-    printf("\n");
+
+    printf("\n%d, %d\n", L.length, L.MaxSize);
+}
+
+void IncreaseList(SqList *L)
+{
+    ElemType *p = L->data;
+    L->data = (ElemType *)malloc(sizeof(ElemType) * (L->MaxSize + IncreaseSize));
+    for (int i = 0; i < L->length; i++)
+    {
+        L->data[i] = p[i];
+    }
+    L->MaxSize = L->MaxSize + IncreaseSize;
+    free(p);
+}
+
+bool ListInsert(SqList *L, ElemType e)
+{
+
+    if (L->length >= L->MaxSize)
+        IncreaseList(L);
+
+    L->data[L->length++] = e;
+
+    return true;
+}
+
+bool ListDelete(SqList *L, ElemType e)
+{
+    bool flag = false;
+    int loc;
+    for (int i = 0; i < L->length; i++)
+    {
+        if (L->data[i] == e)
+        {
+            flag = true;
+            loc = i;
+        }
+    }
+
+    if (loc == L->length)
+        return false;
+
+    for ( ; loc < L->length; loc++)
+    {
+        L->data[loc] = L->data[loc + 1];
+    }
+    L->length--;
+    
+    return true;
     
 }
