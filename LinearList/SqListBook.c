7202ec652d7e3e06c9b57f7e6424049a938a3874
usotsuki <black-sword-k@outlook.com>
2021-07-31 19:25:14 +0800

    Ch2 问题1：书解

--- LinearList/SqListBook.c
+++ LinearList/SqListBook.c
@@ -7,19 +7,20 @@ bool DelMinNum(SqList *L, ElemType *e)
     if (L->length <= 0)
         return false;
 
-    ElemType min = L->data[0];
+    // ElemType min = L->data[0];
+    // 书解
+    (*e) = L->data[0];
     int loc = 0;
 
     for (int i = 1; i < L->length; i++)
     {
-        if (L->data[i] < min)
+        if (L->data[i] < (*e))
         {
-            min = L->data[i];
+            (*e) = L->data[i];
             loc = i;
         }
     }
 
-    (*e) = min;
     L->data[loc] = L->data[--(L->length)];
 
     return true;
